6ed2ad31f40889a6ad5d765bf46472e79586c0f9
Huy Nguyen <huy.nguyen@iswcorp.com>
2015-05-18 15:37:33 -0400

    Add sample maven project

---
+++ src/main/java/accumulo-experience/App.java
@@ -0,0 +1,13 @@
+package accumulo-experience;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
